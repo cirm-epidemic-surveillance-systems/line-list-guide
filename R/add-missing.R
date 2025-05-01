66214bd21afe4af09df7f2787ffe0da5b7e2ba65
Thinh Ong <34956551+thinhong@users.noreply.github.com>
2025-05-01 11:34:32 +0200

    add impute

--- R/add-missing.R
+++ R/add-missing.R
@@ -9,12 +9,12 @@ set.seed(7)
 df <- df |> 
   mutate(
     # missing at random
-    date_onset_miss_rd_20 = ifelse(runif(n()) <= 0.2, NA, date_onset),
+    date_onset_miss_rd = ifelse(runif(n()) <= 0.2, NA, date_onset),
     # impute by date reporting
-    date_onset_miss_rd_20_impute_50 = ifelse(is.na(date_onset_miss_rd_20) & runif(n()) <= 0.5, date_reporting, date_onset_miss_rd_20),
-    date_onset_miss_rd_20_impute_25 = ifelse(is.na(date_onset_miss_rd_20) & runif(n()) <= 0.25, date_reporting, date_onset_miss_rd_20),
-    date_onset_miss_rd_20_impute_75 = ifelse(is.na(date_onset_miss_rd_20) & runif(n()) <= 0.75, date_reporting, date_onset_miss_rd_20),
-    date_onset_miss_rd_20_impute_100 = ifelse(is.na(date_onset_miss_rd_20), date_reporting, date_onset_miss_rd_20),
+    date_onset_miss_rd_impute_50 = ifelse(is.na(date_onset_miss_rd) & runif(n()) <= 0.5, date_reporting, date_onset_miss_rd),
+    date_onset_miss_rd_impute_25 = ifelse(is.na(date_onset_miss_rd) & runif(n()) <= 0.25, date_reporting, date_onset_miss_rd),
+    date_onset_miss_rd_impute_75 = ifelse(is.na(date_onset_miss_rd) & runif(n()) <= 0.75, date_reporting, date_onset_miss_rd),
+    date_onset_miss_rd_impute_100 = ifelse(is.na(date_onset_miss_rd), date_reporting, date_onset_miss_rd),
     # more missing among elderly people
     p_na = rescale(age, to = c(0.05, 0.3)),
     date_onset_miss_age = ifelse(runif(n()) <= p_na, NA, date_onset),
@@ -24,5 +24,10 @@ df <- df |>
     date_onset_miss_age_impute_75 = ifelse(is.na(date_onset_miss_age) & runif(n()) <= 0.75, date_reporting, date_onset_miss_age),
     date_onset_miss_age_impute_100 = ifelse(is.na(date_onset_miss_age), date_reporting, date_onset_miss_age),
   )
+
+tmp <- df |> 
+  filter(!is.na(date_onset_miss_rd), !is.na(date_reporting))
+
+# lm(data = tmp, formula = date_onset_miss_rd ~ date_reporting)
 
 write.csv(df, "../line-list-guide/data-raw/missing_linelist.csv")
