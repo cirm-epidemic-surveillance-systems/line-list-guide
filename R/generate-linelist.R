d79676cbc52bc00e363274f4ea295f89982c3c07
Adrian Lison <adrian.lison@bsse.ethz.ch>
2025-05-01 12:20:11 +0200

    make simulated onset to hosp and death dist explicit

--- R/generate-linelist.R
+++ R/generate-linelist.R
@@ -6,6 +6,8 @@ set.seed(1234)
 
 ll <- sim_linelist(
   outbreak_size = c(1000, 1e+5),
+  onset_to_hosp = function(x) stats::rlnorm(n = x, meanlog = 1.5, sdlog = 0.5),
+  onset_to_death = function(x) stats::rlnorm(n = x, meanlog = 2.5, sdlog = 0.5),
   reporting_delay = \(x) rgamma(n = x, shape = 3, scale = 1)
 )
 write_csv(ll, here("data-raw", "clean_linelist.csv"))
