f6075d20f9603d92909ca9548716f921736c2f13
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-05-01 14:01:03 +0200

    mess up linelist function skeletons

---
+++ R/mess-up-linelist.R
@@ -0,0 +1,31 @@
+impute_missing <- function(ll) {
+
+}
+
+add_missing <- function(ll) {
+
+}
+
+recall_bias <- function(ll) {
+
+}
+
+date_noise <- function(ll) {
+
+}
+
+round_dates <- function(ll) {
+
+}
+
+change_reporting_scheme <- function(ll) {
+
+}
+
+inconsistent_symptom_definitions <- function(ll) {
+
+}
+
+vague_symptom_onset <- function(ll) {
+
+}
